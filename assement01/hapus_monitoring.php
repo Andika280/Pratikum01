0b87ae9fb78669b536a360142a6891efb50da06a
Andika280 <andikaraihan871@gmail,com>
2026-04-28 18:59:00 +0700

    Restore: Mengembalikan struktur folder pekan 8 yang stabil

--- assement01/hapus_monitoring.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require 'koneksi.php';
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $conn->query("DELETE FROM tabel_laporan WHERE id=$id");
-}
-header("Location: dashboard.php");
-?>
